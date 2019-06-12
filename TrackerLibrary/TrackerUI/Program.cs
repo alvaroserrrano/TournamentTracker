b7718a865489559be30f4ebffee1cd9788bfa78a
alvaroserrrano <alvaro.serrano@wmich.edu>
2019-06-12 23:00:47 +0200

    start WinForm

---
+++ TrackerLibrary/TrackerUI/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TrackerUI
+{
+    public class Program
+    {
+        public Program()
+        {
+        }
+    }
+}
---
+++ TrackerLibrary/TrackerUI/TournamentViewerForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace TrackerUI
+{
+    partial class TournamentViewerForm
+    {
+        /// <summary>
+        /// Required designer var
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        /// <summary>
+        /// Clean up every resource being used
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                components,Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
