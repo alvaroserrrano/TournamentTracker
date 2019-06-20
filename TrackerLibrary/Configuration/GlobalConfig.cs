d4ba605a5611d5cbcf324544552625ad7acd2b3a
alvaroserrrano <alvaro.serrano@wmich.edu>
2019-06-20 23:29:41 +0200

    failed DB Conn

--- TrackerLibrary/Configuration/GlobalConfig.cs
+++ TrackerLibrary/Configuration/GlobalConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,10 @@ namespace TrackerLibrary.DataAccess
                 TextConnector text = new TextConnector();
                 Connections.Add(text);
             }
+        }
+        public static string CnnString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
         }
     }
 }
--- TrackerLibrary/DataAccess/SQLConnector.cs
+++ TrackerLibrary/DataAccess/SQLConnector.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 using TrackerLibrary.Models;
 
 namespace TrackerLibrary.DataAccess
@@ -16,10 +18,22 @@ namespace TrackerLibrary.DataAccess
         /// <param name="model">Prize information.</param>
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
-            return model;
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(GlobalConfig.CnnString("Tournaments")))
+            {
+                var p = new DynamicParameters();
+                p.Add("@PlaceNumber", model.PlaceNumber);
+                p.Add("@PlaceName", model.PlaceName);
+                p.Add("@PrizeAmount", model.PrizeAmount);
+                p.Add("@PrizePercentage", model.PrizePercentage);
+                p.Add("@id", 0, DbType.Int32, direction: ParameterDirection.Output);
+
+                connection.Execute("Tournaments.spPrizes_Insert", p, commandType: CommandType.StoredProcedure);
+
+                model.Id = p.Get<int>("@id");
+                return model;
+            }
         }
 
-       
+
     }
 }
